5439269ef3dde8e1fde8028e2cfb525e33eec2b1
He-llo-Wo-rld <serhiiforwork@gmail.com>
2025-09-11 15:48:04 +0100

    refactor(Layout): restructure layout component usage in RootLayout and HomePage

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Layout } from "@/components/Layout/Layout";
 import { Providers } from "@/components/Providers";
 import "./globals.css";
 
@@ -10,7 +11,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="uk">
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          <Layout>{children}</Layout>
+        </Providers>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,12 @@
 "use client";
 
 import { ActionButtons, HeroSection, WelcomeMessage } from "@/components/Home";
-import { Layout } from "@/components/Layout/Layout";
 
 export default function HomePage() {
   return (
-    <Layout>
-      <HeroSection>
-        <WelcomeMessage />
-        <ActionButtons />
-      </HeroSection>
-    </Layout>
+    <HeroSection>
+      <WelcomeMessage />
+      <ActionButtons />
+    </HeroSection>
   );
 }
